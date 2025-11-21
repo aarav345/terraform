932e79ba47a03e7bf9dfe5c02c970a0b3f7f2b6b
aarav <aaravpradhan12365@gmail.com>
2025-11-21 18:03:54 +0545

    added port 9000 in security group

--- aws-devsecops-ec2/security_groups.tf
+++ aws-devsecops-ec2/security_groups.tf
@@ -9,6 +9,13 @@ resource "aws_security_group" "terraform-sg-jenkins" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        from_port = 9000
+        to_port = 9000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
 
     ingress {
         from_port = 22
